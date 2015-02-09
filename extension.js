3a92b2e1a63af5a1c571fd3001f5048933a5d2da
Mark Biek <mark.biek@kindred.com>
2015-02-09 11:12:30 -0500

    Extension is successfully changing the private report links to real links that don't post via js

--- extension.js
+++ extension.js
@@ -5,7 +5,17 @@
     if(title == 'private reports') {
         $('.ed-formTable td a').each( function() {
             if($(this).html().toLowerCase() == 'view') {
-                console.log(this);
+                var oldHref = $(this).attr('href');
+                var res = oldHref.match(/.*?rlViewItm\(['"](.*?)['"].*?/);
+
+                if(res != null && res.length > 1) {
+                    var entId = res[1];
+                    var url = 'https://www.edline.net/DocViewBody.page?currentDocEntid=' + entId + '&returnPage=%2FUserDocList.page';
+                    $(this).attr('href', url);
+                    $(this).css( {
+                        'color': '#009900'
+                    });
+                }
             }
         });
     }
